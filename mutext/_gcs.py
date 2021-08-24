a81aad730705f8c076f4728e5387ffc2ace04ce1
gram <mail@orsinium.dev>
2021-08-24 11:00:26 +0200

    set expiration time

--- mutext/_gcs.py
+++ mutext/_gcs.py
@@ -4,6 +4,9 @@ from gcloud.aio.auth import Token
 from http import HTTPStatus
 from ._exceptions import AlreadyAcquiredError
 from urllib.parse import quote
+from urllib3.filepost import encode_multipart_formdata
+import json
+from datetime import datetime, timedelta
 
 
 DEFAULT_URL = 'https://www.googleapis.com'
@@ -19,6 +22,7 @@ class GCS:
     session: aiohttp.ClientSession
     token: Token
     emulator: bool
+    ttl: timedelta
 
     def __init__(
         self,
@@ -27,11 +31,13 @@ class GCS:
         api_url: Optional[str] = None,
         session: Optional[aiohttp.ClientSession] = None,
         token: Optional[Token] = None,
+        ttl: timedelta = timedelta(seconds=60),
     ) -> None:
         self.bucket = bucket
         self.name = name
         self.emulator = api_url is not None
         self.api_url = api_url or DEFAULT_URL
+        self.ttl = ttl
 
         if session is None:
             session = aiohttp.ClientSession(
@@ -75,19 +81,31 @@ class GCS:
         resp.raise_for_status()
 
     async def _create(self) -> aiohttp.ClientResponse:
+        metadata = {
+            'name': self.name,
+            'expires': (datetime.utcnow() + self.ttl).isoformat(),
+        }
+        body, content_type = encode_multipart_formdata([
+            (
+                {'Content-Type': 'application/json; charset=UTF-8'},
+                json.dumps(metadata).encode('utf-8'),
+            ),
+            (
+                {'Content-Type': 'text/plain'},
+                b'',
+            ),
+        ])
         headers = await self._headers()
         headers.update({
+            'Accept': 'application/json',
             'Content-Length': '0',
-            'Content-Type': '',
+            'Content-Type': content_type,
             'x-goog-if-generation-match': '0',
         })
         return await self.session.post(
             url=f'{self.api_url}/upload/storage/v1/b/{self.bucket}/o',
-            data=b'',
-            params=dict(
-                name=self.name,
-                uploadType='media',
-            ),
+            data=body,
+            params=dict(uploadType='multipart'),
             headers=headers,
         )
 
