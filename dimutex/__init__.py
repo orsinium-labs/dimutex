5a3b0dc36e5ea24864a68db4444c3616adc39af3
gram <mail@orsinium.dev>
2021-12-17 14:03:48 +0100

    bump version

--- dimutex/__init__.py
+++ dimutex/__init__.py
@@ -4,7 +4,7 @@ from ._exceptions import AlreadyAcquiredError, AlreadyReleasedError
 from ._gcs import GCS
 
 
-__version__ = '1.1.0'
+__version__ = '1.2.0'
 __all__ = [
     'AlreadyAcquiredError',
     'AlreadyReleasedError',
